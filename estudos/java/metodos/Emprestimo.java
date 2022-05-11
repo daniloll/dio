c038d7528bf7b3d2bf3fb8952e626f191369d5e4
Lima <danilo.leandro.lima@accenture.com>
2022-05-11 18:01:38 -0300

    Resolucao de Exercicio 2

---
+++ estudos/java/metodos/Emprestimo.java
@@ -0,0 +1,14 @@
+package estudos.java.metodos;
+
+public class Emprestimo {
+
+    public static double simulaValor(double valorInicial, int meses){
+
+        double valorFinal;
+
+        valorFinal = (valorInicial + meses * 2.0);
+
+        return valorFinal;
+
+    }
+}
